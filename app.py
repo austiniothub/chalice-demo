2875a398e0e86eedcaca2a20bd2acdf18786dbf9
Ben Matson <benjamin.matson@live.com>
2017-09-21 11:04:34 -0500

    demo

---
+++ app.py
@@ -0,0 +1,41 @@
+from chalice import Chalice
+import boto3
+
+app = Chalice(app_name='helloworld')
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Instantiate a table resource object without actually
+# creating a DynamoDB table. Note that the attributes of this table
+# are lazy-loaded: a request is not made nor are the attribute
+# values populated until the attributes
+# on the table resource are accessed or its load() method is called.
+table = dynamodb.Table('users')
+
+@app.route('/')
+def index(key={'username': 'janedoe', 'last_name': 'Doe'}):
+    response = table.get_item(Key=key)
+    item = response['Item']
+    return item
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
